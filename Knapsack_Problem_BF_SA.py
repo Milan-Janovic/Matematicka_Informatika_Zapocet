40ce5f26c06cba93bfdbf694b4a8595a85273b5c
Milan-Janovic <milan.janovicml@gmail.com>
2023-04-09 21:31:12 +0200

    Knapsack work continuation

--- Knapsack_Problem_BF_SA.py
+++ Knapsack_Problem_BF_SA.py
@@ -3,12 +3,15 @@ from prettytable import PrettyTable
 import time
 import random
 import numpy as np
+import matplotlib.pyplot as plt
+
 
 # Brute force algorithm to solve the knapsack problem
 def knapsack_brute_force(capacity, items):
-    # Initialize the best value and best subset to 0 and empty, respectively
+    # Initialize the best value, the best subset and fitness_progress  to 0 and empty, respectively
     best_value = 0
     best_subset = []
+    fitness_progress = []
 
     # Iterate over all possible subsets of items
     for i in range(len(items)):
@@ -17,19 +20,23 @@ def knapsack_brute_force(capacity, items):
             subset_weight = sum(item[0] for item in subset)
             subset_value = sum(item[1] for item in subset)
 
-            # Check if the weight is within the capacity of the knapsack and if the value is better than the current best value
+            # Check if the weight is within the capacity of the knapsack
+            # and if the value is better than the current best value
             if subset_weight <= capacity and subset_value > best_value:
                 # Update the best value and best subset
                 best_value = subset_value
                 best_subset = subset
 
-    # Convert the best subset to a list of item numbers and return the best value and best subset
-    return best_value, [items.index(item) + 1 for item in best_subset]
+            fitness_progress.append(best_value)
 
-def knapsack_simulated_annealing(capacity, items, initial_temperature, cooling_rate):
-    # Initialize the current state as an empty subset and the current value as 0
+    # Convert the best subset to a list of item numbers and return the best value and best subset
+    return best_value, [items.index(item) + 1 for item in best_subset], fitness_progress
+
+def knapsack_simulated_annealing(capacity, items, initial_temperature, cooling_rate, alpha=0.8, beta=1.0):
+    # Initialize the best value, the best subset and fitness_progress  to 0 and empty, respectively
     current_subset = []
     current_value = 0
+    fitness_progress = []
 
     # Initialize the best state and the best value to the current state and the current value
     best_subset = current_subset
@@ -39,17 +46,23 @@ def knapsack_simulated_annealing(capacity, items, initial_temperature, cooling_r
     temperature = initial_temperature
     iteration = 0
 
-    # Run the algorithm until the temperature reaches aprox. 0
-    while temperature > 0.001:
+    # Run the algorithm until the temperature reaches the temperature stopping point
+    # or the MAX_ITERS calculated from maxFES of Brute Force algo -> NOT LIKELY TO HAPPEN
+    while temperature > 0.1 and iteration < MAX_ITERS_SA:
+        # print(best_subset)
+        # print(best_value)
         # Select a random neighbor state by randomly adding or removing an item
         neighbor_subset = current_subset[:]
-        if random.random() < 1/len(items) and len(neighbor_subset) < len(items):
+        # Alpha, Beta -> wieghts (bias) for adding/removing and items from subset
+        p_add = min(1, alpha * (len(items) - len(neighbor_subset)) / len(items))
+        p_remove = min(1, beta * len(neighbor_subset) / len(items))
+        if random.random() < p_add and len(neighbor_subset) < len(items):
             # Add a random item to the subset
             available_items = [item for item in items if item not in neighbor_subset]
             if available_items:
                 item_to_add = random.choice(available_items)
                 neighbor_subset.append(item_to_add)
-        elif len(neighbor_subset) > 0 and random.random() < (1 - 1/len(items)):
+        elif len(neighbor_subset) > 0 and random.random() < p_remove:
             # Remove a random item from the subset
             item_to_remove = random.choice(neighbor_subset)
             neighbor_subset.remove(item_to_remove)
@@ -82,45 +95,113 @@ def knapsack_simulated_annealing(capacity, items, initial_temperature, cooling_r
         # Increase the iteration counter
         iteration += 1
 
+        fitness_progress.append(best_value)
+
     # Convert the best subset to a list of item numbers and return the best value and best subset
-    return best_value, [items.index(item) + 1 for item in best_subset]
+    return best_value, [items.index(item) + 1 for item in best_subset], fitness_progress
+
+
+def print_all_items(items):
+    # Print the list of all items
+    table = PrettyTable()
+    table.field_names = ["Item Number", "Weight", "Value"]
+    for i, (weight, value) in enumerate(items):
+        table.add_row([i + 1, weight, value])
+    print("All Items:")
+    print(str(table))
+
+
+def print_solution(items, optimal_items, optimal_value, time_taken):
+    # Create a table of the optimal subset of items
+    table = PrettyTable()
+    table.field_names = ["Item Number", "Weight", "Value"]
+    total_weight = 0
+    for i, (weight, value) in enumerate(items):
+        if i + 1 in optimal_items:
+            table.add_row([i + 1, weight, value])
+            total_weight += weight
+
+    # Print the optimal value, total weight, and table of the optimal subset of items
+    print("Optimal Value:", optimal_value)
+    print("Optimal Weight:", total_weight)
+    print("Optimal Items:")
+    print(str(table))
+    print("Time taken:", time_taken, "seconds")
+
+
+def plot_value_convergence_BF(algo, fitness_progress):
+    strings = []
+    if algo == "BF":
+        strings = ["Knapsack - Brute Force", "KS_BF"]
+    elif algo == "SA":
+        strings = ["Knapsack - Simulated Annealing", "KS_Sa"]
+    else:
+        print("WAT?")
+        return
+
+    plt.plot(fitness_progress)
+    plt.title(strings[0], fontsize=30)
+    plt.xlabel("Number of iterations", fontsize=20)
+    plt.ylabel("Value", fontsize=20)
+    plt.savefig(f"./Knapsack_Fitness_Graphs/{strings[1]}.png")
+    plt.clf()  # clear the figure to avoid overlapping plots
+
+
+def plot_fitness_comparison(func1_name, func2_name, fitness_history1, fitness_history2):
+    plt.plot(fitness_history1, label=func1_name, linewidth=0.5)
+    plt.plot(fitness_history2, label=func2_name, linewidth=0.5)
+    plt.title(f"{func1_name} vs {func2_name} best fitness convergence", fontsize=30)
+    plt.xlabel("Number of iterations", fontsize=20)
+    plt.ylabel("Value", fontsize=20)
+    plt.legend(fontsize=20)
+    plt.savefig(f"./Knapsack_Fitness_Graphs/{func1_name} " f" {func2_name}.png")
+    plt.clf()
+
+
+# Set plot
+plt.figure(figsize=(18, 9))
+
+# Set a number of items to choose from and knapsack capacity
+ITEMS_NUMBER = 30
+CAPACITY = 200
+
+# Set SA parameters
+initial_temperature = 100000
+cooling_rate = 0.99
+
+# Calculater maximal number of subsets for BF, to limit SA maxFES later
+MAX_ITERS_SA = (2**ITEMS_NUMBER) - 1
 
 # Generate a list of random items and a capacity for the knapsack
-items = [(random.randint(1, 50), random.randint(1, 50)) for _ in range(20)]
-capacity = 200
+items = [(random.randint(1, 50), random.randint(1, 50)) for _ in range(ITEMS_NUMBER)]
+
+# Print all items
+print_all_items(items)
+
+
+"""
+
+BF
+
+"""
 
 # Start timer
 start_time = time.time()
 
-# Print the list of all items
-table = PrettyTable()
-table.field_names = ["Item Number", "Weight", "Value"]
-for i, (weight, value) in enumerate(items):
-    table.add_row([i+1, weight, value])
-print("All Items:")
-print(table)
-
 # Solve the knapsack problem using the brute force algorithm
-optimal_value, optimal_items = knapsack_brute_force(capacity, items)
+optimal_value_BF, optimal_items_BF, fitness_progress_BF = knapsack_brute_force(CAPACITY, items)
 
-# Create a table of the optimal subset of items
-table = PrettyTable()
-table.field_names = ["Item Number", "Weight", "Value"]
-total_weight = 0
-for i, (weight, value) in enumerate(items):
-    if i+1 in optimal_items:
-        table.add_row([i+1, weight, value])
-        total_weight += weight
-
-# Print the optimal value, total weight, and table of the optimal subset of items
-print("Optimal Value:", optimal_value)
-print("Optimal Weight:", total_weight)
-print("Optimal Items:")
-print(table)
-
-# End timer and print the time taken to solve the problem
+# End timer and calculate the time taken to solve the problem
 end_time = time.time()
-print("Time taken:", end_time - start_time, "seconds")
+time_taken = end_time - start_time
+
+print("\nBRUTE FORCE")
+
+# Print solution
+print_solution(items, optimal_items_BF, optimal_value_BF, time_taken)
+
+# Plot
+plot_value_convergence_BF("BF", fitness_progress_BF)
 
 """
 
@@ -128,29 +209,32 @@ SA
 
 """
 
+# Solve the knapsack problem using the simulated annealing algorithm
 # Start timer
 start_time = time.time()
 
-# Solve the knapsack problem using the simulated annealing algorithm
-initial_temperature = 100
-cooling_rate = 0.99
-optimal_value, optimal_items = knapsack_simulated_annealing(capacity, items, initial_temperature, cooling_rate)
+optimal_value_SA, optimal_items_SA, fitness_progress_SA = knapsack_simulated_annealing(CAPACITY, items,
+                                                                                           initial_temperature,
+                                                                                           cooling_rate)
 
-# Create a table of the optimal subset of items
-table = PrettyTable()
-table.field_names = ["Item Number", "Weight", "Value"]
-total_weight = 0
-for i, (weight, value) in enumerate(items):
-    if i+1 in optimal_items:
-        table.add_row([i+1, weight, value])
-        total_weight += weight
-
-# Print the optimal value, total weight, and table of the optimal subset of items
-print("Optimal Value:", optimal_value)
-print("Optimal Weight:", total_weight)
-print("Optimal Items:")
-print(table)
-
-# End timer and print the time taken to solve the problem
+# End timer and calculate the time taken to solve the problem
 end_time = time.time()
-print("Time taken:", end_time - start_time, "seconds")
+time_taken = end_time - start_time
+
+print("\nSIMULATED ANNEALING")
+
+# Print solution
+print_solution(items, optimal_items_SA, optimal_value_SA, time_taken)
+
+# Plot
+plot_value_convergence_BF("SA", fitness_progress_SA)
+
+"""
+
+COMPARE -> Not sure if should be done?
+
+"""
+
+# Plot comparison
+# plot_fitness_comparison("BF", "SA", fitness_progress_BF, fitness_progress_SA)
+
